dc249f443a561d132822072361ffd66ddd265833
Johannes Maximilian Toball <code@gedankenacker.de>
2012-03-08 17:08:09 +0100

    Missing files…

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quodes::Application.config.secret_token = '79792cc30ab8aeeb3abad8b5a0e1de8a9f5159c6d53498580f6f2d6b878bc232a6e1da435670179bce7955f943b5810e9b6ae3e4da402e6d754d33052e99160d'
